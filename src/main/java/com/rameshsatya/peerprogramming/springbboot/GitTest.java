08aace4df7a36c5366c0e26daf2f64fbf2865b43
satyakadali <satyakadali@localhost>
2021-01-09 21:44:31 -0500

    Added sysyout

--- src/main/java/com/rameshsatya/peerprogramming/springbboot/GitTest.java
+++ src/main/java/com/rameshsatya/peerprogramming/springbboot/GitTest.java
@@ -3,6 +3,7 @@ package com.rameshsatya.peerprogramming.springbboot;
 public class GitTest {
 public static void main(String[] args) {
 	System.out.println("git test");
+	System.out.println("git test");
 	
 }
 }
